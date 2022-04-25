bf5ce01c1aa127833b06f924fff73fb47670e9bd
Erick Gonçalves <93154145+erickgcsilva@users.noreply.github.com>
2022-04-25 17:53:42 -0300

    Desenvolvimento Azin

---
+++ Teste/Teste/Grand Chase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Grand_Chase
+    {
+        public string Azin { get; set; }
+        public string Sieghart { get; set; }
+        public string Lupus { get; set; }
+        public string Dio { get; set; }
+    }
+}
